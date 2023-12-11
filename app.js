bd216a19cb6bce1b31e3c173c9b71f3ced6bfbf2
dev-dragon1022 <liueejust@yahoo.com>
2023-12-12 01:01:24 +0800

    111

---
+++ app.js
@@ -0,0 +1,27 @@
+// by Just. Liu
+// v0.0.1
+
+const app = Vue.createApp({	
+  data() {
+    return {
+			
+    }
+  },
+  mounted() {
+		
+	},
+	computed: {
+		
+	},
+  methods: {
+		// 回到顶部
+		// top(){
+		// 	window.scrollTo({
+		// 	        top:0,
+		// 	        behavior:"smooth"
+		// 	    })
+		// }
+  }
+})
+
+app.mount('#app')
