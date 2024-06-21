284e0ed950e2691d67eb2e47da63b803ce1123b5
dev-dragon1022 <liueejust@yahoo.com>
2024-06-21 16:05:42 +0800

    updata

--- app.js
+++ app.js
@@ -4,10 +4,19 @@
 const app = Vue.createApp({	
   data() {
     return {
-			
+			items: [],
+			designType: 0,
+			designClass: 0,
+			devType: 1,
+			devClass: 0
     }
   },
   mounted() {
+		axios.get('data/data.json').then(response => {
+			this.items = response.data;
+		}).catch(error => {
+			console.log(error);
+		});
 		
 		// 显示公众号pop
 		var aTag = document.querySelector('.footer-gzh');
@@ -22,8 +31,29 @@ const app = Vue.createApp({
 	},
 	computed: {
 		
+		// 设计数据items
+		designItems() {
+				return this.items.filter(item => item.type === this.designType && item.class === this.designClass).slice().reverse();
+		},
+		
+		// 前端数据items
+		devItems() {
+				return this.items.filter(item => item.type === this.devType && item.class === this.devClass).slice().reverse();
+		}
+		
+		
 	},
   methods: {
+		// 设计tab
+		selectDesignTab(type, clazz) {
+			this.designType = type;
+			this.designClass = clazz;
+		},
+		// 前端tab
+		selectDevTab(type, clazz) {
+			this.devType = type;
+			this.devClass = clazz;
+		}
 		// 回到顶部
 		// top(){
 		// 	window.scrollTo({
